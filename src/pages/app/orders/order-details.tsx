bd10aeb427a479b235c7ba6b52000728e2c3a550
Pedro <pedrodelmonico@hotmail.com>
2024-05-09 10:07:16 -0300

    finished order section

--- src/pages/app/orders/order-details.tsx
+++ src/pages/app/orders/order-details.tsx
@@ -4,7 +4,15 @@ import {
   DialogHeader,
   DialogTitle,
 } from '@/components/ui/dialog'
-import { Table, TableBody, TableCell, TableRow } from '@/components/ui/table'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
 
 export function OrderDatails() {
   return (
@@ -55,6 +63,38 @@ export function OrderDatails() {
               <TableCell className="flex justify-end">3 minutos</TableCell>
             </TableRow>
           </TableBody>
+        </Table>
+
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Produtos</TableHead>
+              <TableHead className="text-right">Qtd.</TableHead>
+              <TableHead className="text-right">Preço</TableHead>
+              <TableHead className="text-right">Subtotal</TableHead>
+            </TableRow>
+          </TableHeader>
+
+          <TableBody>
+            <TableRow>
+              <TableCell>Pizza Pepperoni Familia</TableCell>
+              <TableCell className="text-right">2</TableCell>
+              <TableCell className="text-right">R$ 69,90</TableCell>
+              <TableCell className="text-right">R$ 139,80</TableCell>
+            </TableRow>
+
+            <TableRow>
+              <TableCell>Pizza Mussarela Familia</TableCell>
+              <TableCell className="text-right">2</TableCell>
+              <TableCell className="text-right">R$ 59,90</TableCell>
+              <TableCell className="text-right">R$ 119,80</TableCell>
+            </TableRow>
+          </TableBody>
+
+          <TableFooter>
+            <TableCell colSpan={3}>Total do pedido</TableCell>
+            <TableCell className="text-right font-medium">R$ 259,60</TableCell>
+          </TableFooter>
         </Table>
       </div>
     </DialogContent>
